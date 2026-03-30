b5efc17cc218ba37891701661f142bd50079d554
fractal-fusion <fractal.fusion27188@gmail.com>
2026-03-30 18:26:09 -0400

    feat(Achievements): Update and expand achievement data
    Includes new achievements for 2026 and refines existing entries to provide more specific details, enhancing the display of team accomplishments.

--- components/Achievements.tsx
+++ components/Achievements.tsx
@@ -16,7 +16,9 @@ const seasonTwo = [
   { year: '2025', event: 'Meet 3', rank: '15 Artifacts' },
   { year: '2026', event: 'League Champs', rank: 'Winning Alliance Capt.' },
   { year: '2026', event: 'Design Award', rank: '2nd Place' },
-  { year: '2026', event: 'State Champ', rank: 'Loading...' },
+  { year: '2026', event: 'State Champ', rank: 'Winning Alliance' },
+  { year: '2026', event: 'Design Award', rank: '2nd Place, State Level' },
+  { year: '2026', event: 'World Champs', rank: 'Loading...' },
 ];
 
 export const Achievements: React.FC = () => {
